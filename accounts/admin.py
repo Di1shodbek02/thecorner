2e35985ce71783c143d27413086e3b065ef7b6b0
Dil1shodbek02 <ahmatovdilshodbek@gmail.com>
2024-01-17 15:44:35 +0500

    change

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 
+from accounts.models import Role, UserRole
 from main.models import Product
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UserRole)
+class UserRoleAdmin(admin.ModelAdmin):
     pass
